e7f5158310c43c5fdb7813f7dcb9553e96d44263
mpie314 <MarekPietrzyk@NorthwesternMutual.com>
2015-03-18 21:16:48 -0500

    Initial changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To create an equivalent of pretty much instantenous computations of matrix
+## inverse, cache the results of the first invocation, so that subsequent
+## invocations of the function do not have to repeat the computation.
+## The function under consideration here is computing the inverse of a square
+## matrix.
 
-## Write a short comment describing this function
+
+## Define a list, which is a kind of partially initialized singleton object
+## containing data and functions setting and getting those data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    X <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Define a function to perform the desired computation (matrix inversion)
+## in the first invocation, but reading the cached result in subsequent
+## invocations.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if ( !is.null(m) ) {
+    message("Getting cached result")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
